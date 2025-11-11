6df9e958cd05521e559a16eee79189f10065301d
clement lion <clement.lionceau@gmail.com>
2025-11-11 16:58:10 +0100

    added AssetManager initial code

---
+++ Game/Services/AssetManager.py
@@ -0,0 +1,9 @@
+"""
+An object to handle Game Assets.
+"""
+from Game.Services.Singleton import SingletonMeta
+
+
+class AssetManager(metaclass=SingletonMeta):
+    def __init__(self) -> None:
+        pass
